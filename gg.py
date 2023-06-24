5f01e635ee074b7681dc8215de9a6141d473777a
PIRAVEEN44 <137202733+PIRAVEEN44@users.noreply.github.com>
2023-06-24 15:35:34 +0530

    Add files via upload

---
+++ gg.py
@@ -0,0 +1,16 @@
+print('Hello world!')
+person=input('what is your name?')
+('Hello',person)
+Hello world!
+what is your name?piraveen
+Hello piraveen
+>>> age=14
+>>> print(age)
+14
+>>> Weight=49
+>>> print(Weight)
+49
+>>> Height=162
+>>> print(Height)
+162
+>>>  
